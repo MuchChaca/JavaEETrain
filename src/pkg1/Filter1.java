4898ca1117d16255d76d91a43110ef188e6db389
Hell <bastide.seb.pro@gmail.com>
2018-04-10 10:10:29 +0400

    Filters

---
+++ src/pkg1/Filter1.java
@@ -0,0 +1,63 @@
+package pkg1;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * Servlet Filter implementation class Filter1
+ */
+@WebFilter({ "/Filter1", "/*" })
+public class Filter1 implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public Filter1() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+		
+		int age;
+		
+		try {
+			age = Integer.valueOf(request.getParameter("age"));
+		} catch(Exception ex) {
+			age = 0;
+		}
+		
+		
+		// pass the request along the filter chain
+		if (age < 18 ) {
+			chain.doFilter(request, response);
+		} else {
+			response.getWriter().print("Too old.");
+		}
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
